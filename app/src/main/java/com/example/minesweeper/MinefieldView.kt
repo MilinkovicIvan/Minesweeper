5138e11566a2c0a03ad34531e9d9edc7b82f910c
MilinkovicIvan <ivan.milinkovic91@gmail.com>
2021-11-28 01:51:20 +0000

    Milestone 2, drawing 10x10 board with covered cells

--- app/src/main/java/com/example/minesweeper/MinefieldView.kt
+++ app/src/main/java/com/example/minesweeper/MinefieldView.kt
@@ -9,7 +9,7 @@ import android.view.View
 
 class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val paint = Paint()
-    //private var size = 0
+    private var size = 100f
 
     override fun onDraw(canvas: Canvas) {
         // call the super method to keep any drawing from the parent side.
@@ -18,17 +18,27 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
         drawMinefield(canvas)
     }
 
+    // function that will draw minefield in app
     private fun drawMinefield(canvas: Canvas) {
         //mine border
         paint.color= Color.WHITE
         paint.style = Paint.Style.FILL
         canvas.drawRect(0f, 0f, 1000f, 1000f, paint)
 
+        //columns
+        for (j in 0..9){
+            //rows
+            for(i in 0..9){
+                //mine background
+                paint.color= Color.BLACK
+                paint.style = Paint.Style.FILL
+                canvas.drawRect(5f + i * size, 5f + j * size, 95f + i * size, 95f + j * size, paint)
+            }
+        }
 
-        //mine background
-        paint.color= Color.BLACK
-        paint.style = Paint.Style.FILL
-        canvas.drawRect(5f, 5f, 995f, 995f, paint)
+
+
+
 
     }
 }
