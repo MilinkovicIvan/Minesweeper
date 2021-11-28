b958b04196965736917f652d7f59b1dfddbfa8d7
MilinkovicIvan <ivan.milinkovic91@gmail.com>
2021-11-28 06:02:18 +0000

    Milestone 3, cell is now uncovered when user clicks on minefield, cell background changes from black to gray

--- app/src/main/java/com/example/minesweeper/MinefieldView.kt
+++ app/src/main/java/com/example/minesweeper/MinefieldView.kt
@@ -1,20 +1,32 @@
 package com.example.minesweeper
 
+
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.MotionEvent
+
 
 class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val paint = Paint()
     private var size = 100f
 
+    //user click coordinates
+    private var x = -1
+    private var y = -1
+
+    //2d array used to store cell values, 0 for covered,1 for uncovered
+    private val gridArray = Array(10, { IntArray(10) })
+
+
+
     override fun onDraw(canvas: Canvas) {
         // call the super method to keep any drawing from the parent side.
         super.onDraw(canvas)
-
         drawMinefield(canvas)
     }
 
@@ -25,20 +37,52 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
         paint.style = Paint.Style.FILL
         canvas.drawRect(0f, 0f, 1000f, 1000f, paint)
 
-        //columns
-        for (j in 0..9){
-            //rows
-            for(i in 0..9){
-                //mine background
-                paint.color= Color.BLACK
-                paint.style = Paint.Style.FILL
-                canvas.drawRect(5f + i * size, 5f + j * size, 95f + i * size, 95f + j * size, paint)
+        var row = 0
+        var column = 0
+        //rows
+        for (j in gridArray){
+            //columns
+            for(i in j){
+                // if value on grid is 1 that will draw uncovered cell
+                if(gridArray[row][column] == 1) {
+                    //mine background
+                    paint.color= Color.LTGRAY
+                    paint.style = Paint.Style.FILL
+                    canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
+                    column++
+                }
+                // value is 0 and it covered cell will be drawn
+                else{
+                    //mine background
+                    paint.color= Color.BLACK
+                    paint.style = Paint.Style.FILL
+                    canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
+                    column++
+                }
+            }
+            column = 0
+            row++
+        }
+    }
+
+    //function that will provide coordinates of user clicking on cells
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                x = event.x.toInt()
+                y = event.y.toInt()
+                //Log.i("TAG", x.toString() + " x")
+                //Log.i("TAG", y.toString() + " y")
+
+                // only if user click inside canvas gridArray will be updated
+                if(x < 1000 && y < 1000){
+                    // changes cell from covered to uncovered
+                    gridArray[y/100][x/100] = 1
+                    invalidate()
+                }
             }
         }
-
-
-
-
-
+        return true
     }
+
 }
