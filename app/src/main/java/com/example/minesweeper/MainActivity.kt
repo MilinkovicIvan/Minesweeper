92d6f333de95140ae3b82c0ddfa6e785b4f7e217
MilinkovicIvan <ivan.milinkovic91@gmail.com>
2021-11-29 04:49:53 +0000

    Milestone 4, 20 random mines have been implemented,canvas now draws red cells with M to represent mines

--- app/src/main/java/com/example/minesweeper/MainActivity.kt
+++ app/src/main/java/com/example/minesweeper/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.minesweeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var minefieldView: MinefieldView
+    private lateinit var totalMines: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        minefieldView = findViewById<MinefieldView>(R.id.minefieldView)
+        totalMines = findViewById<TextView>(R.id.totalMines)
+
+        minefieldView.setTotalMines(totalMines)
     }
 }
--- app/src/main/java/com/example/minesweeper/MinefieldView.kt
+++ app/src/main/java/com/example/minesweeper/MinefieldView.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.view.MotionEvent
+import android.widget.TextView
 
 
 class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -19,8 +20,14 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
     private var x = -1
     private var y = -1
 
+    private var totalMinesUpdate: TextView? = null
+
     //2d array used to store cell values, 0 for covered,1 for uncovered
     private val gridArray = Array(10, { IntArray(10) })
+
+    //2d array used to store mines on field, 0 for no mine,1 for mine
+    private val mineArray = Array(10, { IntArray(10) })
+
 
 
 
@@ -28,14 +35,23 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
         // call the super method to keep any drawing from the parent side.
         super.onDraw(canvas)
         drawMinefield(canvas)
+
     }
 
     // function that will draw minefield in app
     private fun drawMinefield(canvas: Canvas) {
-        //mine border
+        //adding mines to field
+        var mineCount = checkMineAmount(mineArray)
+        if(mineCount == 0){
+            addMines(mineArray)
+        }
+
+        //cell border
         paint.color= Color.WHITE
         paint.style = Paint.Style.FILL
         canvas.drawRect(0f, 0f, 1000f, 1000f, paint)
+
+        var mineHit = false
 
         var row = 0
         var column = 0
@@ -45,23 +61,50 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
             for(i in j){
                 // if value on grid is 1 that will draw uncovered cell
                 if(gridArray[row][column] == 1) {
-                    //mine background
-                    paint.color= Color.LTGRAY
-                    paint.style = Paint.Style.FILL
-                    canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
-                    column++
+                    //check if the cell is a mine
+                    if(mineArray[row][column] == 1) {
+                        //mine background
+                        paint.color= Color.RED
+                        paint.style = Paint.Style.FILL
+                        canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
+                        paint.color= Color.BLACK
+                        paint.textSize = 50f
+                        var text = "M"
+                        canvas.drawText(text,(5f + column * size)+25, (5f + row * size)+60, paint)
+                        column++
+
+                        //flag to see if mine is hit,if true than reveal all mines
+                        mineHit = true
+                        if (mineHit == true){
+                            revealMines(canvas)
+                        }
+                    }
+                    else{
+                        //uncovered cell background
+                        paint.color= Color.LTGRAY
+                        paint.style = Paint.Style.FILL
+                        canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
+                        column++
+                    }
                 }
                 // value is 0 and it covered cell will be drawn
                 else{
-                    //mine background
+                    //covered cell background
                     paint.color= Color.BLACK
                     paint.style = Paint.Style.FILL
                     canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
                     column++
                 }
+
+
+
             }
             column = 0
             row++
+        }
+
+        if (mineHit == true){
+            revealMines(canvas)
         }
     }
 
@@ -74,6 +117,11 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
                 //Log.i("TAG", x.toString() + " x")
                 //Log.i("TAG", y.toString() + " y")
 
+                //mine count
+                var mineCount = checkMineAmount(mineArray)
+                //update total mines value
+                totalMinesUpdate?.setText("Mines: " + mineCount.toString())
+
                 // only if user click inside canvas gridArray will be updated
                 if(x < 1000 && y < 1000){
                     // changes cell from covered to uncovered
@@ -83,6 +131,72 @@ class MinefieldView(context: Context, attrs: AttributeSet) : View(context, attrs
             }
         }
         return true
+    }
+    //function used to update totalMines textView
+    fun setTotalMines(totalMines: TextView?) {
+        totalMinesUpdate = totalMines
+    }
+
+    //function which will add 20 mines randomly to mineArray
+    fun addMines(arr: Array<IntArray>){
+        var mineCount = 0
+        for (i in 1..100000) {
+            if (mineCount < 20){
+                val row = (0..9).random() // generated random from 0 to 9
+                val column = (0..9).random()
+                if(arr[row][column] == 0){
+                    arr[row][column] = 1
+                    mineCount++
+                }
+            }
+        }
+    }
+
+    //function which will check how many mines are in array
+    fun checkMineAmount(arr: Array<IntArray>): Int {
+        var count = 0
+
+        var row = 0
+        var column = 0
+        //rows
+        for (j in arr){
+            //columns
+            for(i in j){
+                // if value on grid is 1 that means mine found
+                if(arr[row][column] == 1) {
+                    count++
+
+                }
+                column++
+            }
+            column = 0
+            row++
+        }
+        return count
+    }
+
+    fun revealMines(canvas: Canvas){
+        var row = 0
+        var column = 0
+        //rows
+        for (j in mineArray){
+            //columns
+            for(i in j){
+                // if value on grid is 1 that means mine found
+                if(mineArray[row][column] == 1) {
+                    paint.color= Color.RED
+                    paint.style = Paint.Style.FILL
+                    canvas.drawRect(5f + column * size, 5f + row * size, 95f + column * size, 95f + row * size, paint)
+                    paint.color= Color.BLACK
+                    paint.textSize = 50f
+                    var text = "M"
+                    canvas.drawText(text,(5f + column * size)+25, (5f + row * size)+60, paint)
+                }
+                column++
+            }
+            column = 0
+            row++
+        }
     }
 
 }
